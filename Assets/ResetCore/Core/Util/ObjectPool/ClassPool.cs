523283545abb4f6fbbbb20ab926e0fb1294bdd3b
DC <352070977@qq.com>
2017-07-16 19:38:14 +0800

    更新

---
+++ Assets/ResetCore/Core/Util/ObjectPool/ClassPool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ResetCore.Util
+{
+    public class ClassPool
+    {
+
+        //private Dictionary<Type, Stack<IStorable>> stackMap = new 
+        
+        
+
+    }
+
+    /// <summary>
+    /// 可储存的对象
+    /// </summary>
+    public interface IStorable
+    {
+        bool isDirty { get; set; }
+        void Reset();
+        void StoreToPool();
+    }
+}
