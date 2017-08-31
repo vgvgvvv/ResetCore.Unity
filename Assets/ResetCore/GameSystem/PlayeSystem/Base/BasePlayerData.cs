d68a5809282c84d55e8915aaf45bc5af05785820
dc <352070977@qq.com>
2017-08-31 20:20:43 +0800

    更新U3D，添加PlayerSystem

---
+++ Assets/ResetCore/GameSystem/PlayeSystem/Base/BasePlayerData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+namespace ResetCore.GameSystem
+{
+    public class BasePlayerData
+    {
+        public float speed = 1;
+        public float delayTime = 0;
+
+        public BasePlayerData(float speed, float delayTime)
+        {
+            this.speed = speed;
+            this.delayTime = delayTime;
+        }
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <typeparam name="TPlayer"></typeparam>
+        /// <param name="player"></param>
+        /// <returns></returns>
+        public string ToJson()
+        {
+            return JsonMapper.ToJson(this);
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <typeparam name="TData"></typeparam>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public TData FromJson<TData>(string json) where TData : BasePlayerData
+        {
+            return JsonMapper.ToObject<TData>(json);
+        }
+    }
+}
---
+++ Assets/ResetCore/GameSystem/PlayeSystem/TimePlayer/TimePlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ResetCore.GameSystem
+{
+    public abstract class TimePlayer : BasePlayer<TimePlayerData>
+    {
+        public float duration { get; private set; }
+
+        public override void Init(TimePlayerData playerData, BasePlayer[] playerList = null)
+        {
+            base.Init(playerData, playerList);
+            duration = playerData.duration;
+        }
+
+        protected override bool CheckEnd()
+        {
+            return pastTime >= duration;
+        }
+    }
+}
