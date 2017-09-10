412b82eb6fa7e42c1368693ac20ae25d0cf010c1
DC <352070977@qq.com>
2017-09-11 06:18:52 +0800

    对目录进行初级的重新分层，并且删除了部分无用的PathConfig，接下去还要讲所有的加载方式更换为IBundleLoader

---
+++ Assets/ResetCore/Engine/DllManager/DllManagerConst.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using ResetCore.Util;
+
+namespace ResetCore.ReAssembly
+{
+    public static class DllManagerConst
+    {
+        public static readonly string scriptableCSOutputPath = PathEx.Combine("Engine/DllManager", "SciptableLoader/ScriptableObjects");
+    }
+
+}
